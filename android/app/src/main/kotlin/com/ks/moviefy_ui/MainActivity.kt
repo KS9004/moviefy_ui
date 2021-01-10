79270631f60e600a081e086ddcbb8e9349a6f5d3
Kumar Saurabh <55243385+KS9004@users.noreply.github.com>
2021-01-10 20:57:00 +0530

    Intial commit

---
+++ android/app/src/main/kotlin/com/ks/moviefy_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ks.moviefy_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:moviefy_ui/screens/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Moviefy',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      home: HomeScreen()
+    );
+  }
+}
+
