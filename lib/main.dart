3677933df9f522d6110870ed65a3eadfbfe30658
Kumar Saurabh <55243385+KS9004@users.noreply.github.com>
2021-01-12 19:43:57 +0530

    given title for featureMovie

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark
       ),
-      home: HomeScreen()
+      home: Scaffold(body: HomeScreen())
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,6 +9,14 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   var _deviceHeight;
   var _deviceWidth;
+  var _selectedMovie;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _selectedMovie = 0;
+  }
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
@@ -16,13 +24,19 @@ class _HomeScreenState extends State<HomeScreen> {
     return Stack(
       children: [
       _featuredMovieWidget(),
-      _gradientWidget()
+      _gradientWidget(),
+        _topLayerWidget()
  ]
     );
   }
   Widget _featuredMovieWidget(){
     return SizedBox(height: _deviceHeight*0.50,width:_deviceWidth ,
       child: PageView(
+        onPageChanged: (_index){
+          setState(() {
+            _selectedMovie = _index;
+          });
+        },
         scrollDirection:Axis.horizontal,
         children:
           featureMovies.map((_movie){
@@ -54,6 +68,87 @@ class _HomeScreenState extends State<HomeScreen> {
             end: Alignment.topCenter,
           )
         ),
+      ),
+    );
+  }
+  Widget _topLayerWidget (){
+    return Padding(
+      padding:  EdgeInsets.symmetric(
+          horizontal: _deviceWidth*0.05,
+          vertical: _deviceHeight*0.005),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _topBarWidget(),
+          SizedBox(height: _deviceHeight * 0.13,),
+          _FeaturedMoviesInfoWidget()
+        ],
+      ),
+    );
+  }
+  Widget _topBarWidget(){
+    return SizedBox(
+      height: _deviceHeight*0.13,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(Icons.menu,
+          color: Colors.white,
+          size: 30,),
+          Row(
+            children: [
+              Icon(Icons.search,
+                color: Colors.white,
+                size: 30,),
+              SizedBox(width: _deviceWidth*0.03,),
+              Icon(Icons.notifications_none,
+                color: Colors.white,
+                size: 30,)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+  Widget _FeaturedMoviesInfoWidget(){
+    return SizedBox(
+      height: _deviceHeight*0.12,
+      width: _deviceWidth,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(featureMovies[_selectedMovie].title,
+          maxLines: 2,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: _deviceHeight*0.040
+          ),),
+          SizedBox(height: _deviceHeight * 0.01,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: featureMovies.map((_movie){
+              bool _isActive = _movie.title == featureMovies[_selectedMovie].title;
+              double _circleRadius = _deviceHeight * 0.004;
+              return Container(
+                margin: EdgeInsets.only(right: _deviceWidth * 0.015),
+                height: _circleRadius * 2,
+                width: _circleRadius * 2,
+                decoration: BoxDecoration(
+                  color: _isActive ? Colors.grey:Colors.green,
+                  borderRadius: BorderRadius.circular(100)
+                ),
+              );
+            }).toList(),
+          )
+        ],
       ),
     );
   }
